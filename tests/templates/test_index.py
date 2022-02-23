6880cef7d5552660240fc8c25fa11c2b7684794e
LiTiBe <contact@litiboost.fr>
2022-02-23 13:10:11 +0100

    add structure app for testing

---
+++ tests/templates/test_index.py
@@ -0,0 +1,25 @@
+import pytest
+from server import create_app
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+def test_request_index(client):
+    response = client.get("/")
+    assert b"<h1>Welcome to the GUDLFT Registration Portal!</h1>" in response.data
+    assert response.status_code == 200
