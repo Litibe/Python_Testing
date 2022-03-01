778b5598509ddf379ce4c7fa34ef47ec0485fb4a
LiTiBe <contact@litiboost.fr>
2022-03-01 08:17:49 +0100

    add tests

--- tests/tests_unitaires/controller/test_server.py
+++ tests/tests_unitaires/controller/test_server.py
@@ -1,7 +1,7 @@
 from http import server
 import datetime
 import pytest
-from server import create_app, load_datime_now
+from server import create_app, load_datime_now, loadClubs, loadCompetitions, saveClubs, saveCompetitions
 
 
 @pytest.fixture()
@@ -23,3 +23,26 @@ def test_date_now():
     now = datetime.datetime.now()
     now = now.strftime("%Y-%m-%d %H:%M:%S")
     assert date_now == now
+
+
+def test_load_clubs():
+    listOfClubs = loadClubs()
+    assert isinstance(listOfClubs, list)
+
+
+def test_save_clubs():
+    clubs = loadClubs()
+    if len(clubs) > 0:
+        assert saveClubs(clubs[0]['name'], clubs[0]['points'])
+
+
+def test_load_competition():
+    listOfCompetitions = loadCompetitions()
+    assert isinstance(listOfCompetitions, list)
+
+
+def test_save_competitions():
+    competitions = loadCompetitions()
+    if len(competitions) > 0:
+        assert saveCompetitions(
+            competitions[0]['name'], competitions[0]['numberOfPlaces'])
