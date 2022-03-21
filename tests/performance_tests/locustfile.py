47f897965b14a75e09dca3014051ccd2f58e126e
LiTiBe <contact@litiboost.fr>
2022-03-21 16:27:56 +0100

    fix locust

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -1,28 +1,18 @@
 import json
 import time
 from random import randint
-from locust import HttpUser, between, task
+from locust import HttpUser, task
 from server import loadClubs, loadCompetitions, loadBooking, load_datime_now
-
+import CONSTANTS
 listOfClubs = loadClubs()
 listOfCompetitions = loadCompetitions()
 listOfbooking = loadBooking()
 
 
-def erase_test_into_json_file():
-    with open('clubs.json', "w") as file:
-        json.dump({'clubs': listOfClubs}, file, indent=4)
-    with open('competitions.json', "w") as file:
-        json.dump({'competitions': listOfCompetitions},
-                  file, indent=4)
-    with open('booking.json', "w") as file:
-        json.dump({'booking': listOfbooking}, file, indent=4)
-
-
 def search_club_available():
     clubs = loadClubs()
     for club in clubs:
-        if int(club["points"]) > 3:
+        if int(club["points"]) >= CONSTANTS.POINTS_PER_PLACE:
             return club
     return ""
 
@@ -40,62 +30,19 @@ def search_competiton_available():
         return ""
 
 
-def on_start():
-    with open('competitions.json', "w") as file:
-        json.dump({
-            "competitions": [
-                {
-                    "name": "Spring Festival",
-                    "date": "2022-03-27 10:00:00",
-                    "numberOfPlaces": "25"
-                },
-                {
-                    "name": "Fall Classic",
-                    "date": "2022-10-22 13:30:00",
-                    "numberOfPlaces": "13"
-                }
-            ]
-        },
-            file, indent=4)
-    with open('clubs.json', "w") as file:
-        json.dump({
-            "clubs": [
-                {
-                    "name": "Simply Lift",
-                    "email": "john@simplylift.co",
-                    "points": "13"
-                },
-                {
-                    "name": "Iron Temple",
-                    "email": "admin@irontemple.com",
-                    "points": "4"
-                },
-                {
-                    "name": "She Lifts",
-                    "email": "kate@shelifts.co.uk",
-                    "points": "12"
-                }
-            ]
-        },
-            file, indent=4)
-    return True
-
-
 class ServerPerfTest(HttpUser):
-    wait_time = between(1, 2)
-
-    @task(3)
+    @task()
     def home(self):
         self.client.get("/")
 
-    @task(3)
+    @task()
     def login(self):
         clubs = loadClubs()
         params = {"email": listOfClubs[randint(
             0, len(clubs)-1)]["email"]}
         self.client.post('/showSummary', data=params)
 
-    @task(2)
+    @task()
     def show_booking_page(self):
         competition_valid = search_competiton_available()
         clubs = loadClubs()
@@ -107,7 +54,7 @@ class ServerPerfTest(HttpUser):
             url += club_name[0]+"%20"+club_name[1]
             self.client.get(url)
 
-    @task(1)
+    @task()
     def booking_places(self):
         club_with_places = search_club_available()
         competition_valid = search_competiton_available()
@@ -119,6 +66,6 @@ class ServerPerfTest(HttpUser):
             url = "/purchasePlaces"
             self.client.post(url, data=data)
 
-    @task(3)
+    @task()
     def logout(self):
         self.client.get('/logout')
